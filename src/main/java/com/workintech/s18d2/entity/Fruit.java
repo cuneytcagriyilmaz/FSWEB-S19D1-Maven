fc7d29c1e2c4a6e3c30fb79c9101acc9aa3fe0ee
C. Çağrı YILMAZ <cuneytcagriyilmaz@hotmail.com>
2024-05-06 19:07:38 +0300

    s19 d1

--- src/main/java/com/workintech/s18d2/entity/Fruit.java
+++ src/main/java/com/workintech/s18d2/entity/Fruit.java
@@ -2,11 +2,13 @@ package com.workintech.s18d2.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "fruit")
 public class Fruit {
--- src/main/java/com/workintech/s18d2/entity/Vegetable.java
+++ src/main/java/com/workintech/s18d2/entity/Vegetable.java
@@ -1,11 +1,13 @@
 package com.workintech.s18d2.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vegetable")
 public class Vegetable {
